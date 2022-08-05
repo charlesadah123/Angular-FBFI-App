9187bb982b9b049b4291dd2dc0b92ecdc78fdbb8
charles <charlesadah123@gmail.com>
2022-08-05 08:08:57 -0700

    per

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fbfi'));
+app.use(express.static(__dirname + '/app/dist/fbfi'));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + "/dist/fbfi/index.html"));
+    res.sendFile(path.join(__dirname + "/app/dist/fbfi/index.html"));
 } );
 
 // Start the app by listening on the default Heroku port
