48630981858def8b52f1b0220675bb5cb8c11ee4
charles <charlesadah123@gmail.com>
2022-08-05 05:01:34 -0700

    package.json is adjusted

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/landingpagetestfbfi-master'));
+app.use(express.static('./dist/fbfi'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/landingpagetestfbfi-master/'}),
+    res.sendFile('index.html', {root: 'dist/fbfi/'}),
 );
 
 // Start the app by listening on the default Heroku port
